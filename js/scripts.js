5db93159cc2963fc86ae2ad3bb77a499c7777c7d
objetosdeaprendizaje <daniela.na1208@gmail.com>
2021-07-12 12:26:29 -0500

    dia doce

--- js/scripts.js
+++
@@ -1,9 +0,0 @@
-
-const cards = document.querySelectorAll ('.memory-card');
-
-function flipCard(){
-     console.log('vamos a ver');
-    console.log('ver');
-}
-
-cards.forEach(card => card.addEventListener('click',flipCard));
