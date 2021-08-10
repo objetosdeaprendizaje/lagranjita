f9687903bb5dc58878d7598b56f4a42762fa199f
objetosdeaprendizaje <daniela.na1208@gmail.com>
2021-08-09 20:46:31 -0500

    cambios de hoy

---
+++ js/actividad2.js
@@ -0,0 +1,77 @@
+const form = document.getElementById('form');
+var uno = document.getElementById('uno');
+const dos = document.getElementById('dos');
+const tres = document.getElementById('tres');
+const cuatro = document.getElementById('cuatro');
+const cinco = document.getElementById('cinco');
+const seis = document.getElementById('seis');
+const siete = document.getElementById('siete');
+
+
+form.addEventListener('submit', (e) =>{
+    e.preventDefault();
+
+    checkInputs();
+});
+
+function checkInputs(){
+    var unoValue = uno.value;
+    const dosValue = dos.value;
+    const tresValue = tres.value;
+    const cuatroValue = cuatro.value;
+    const cincoValue = cinco.value;
+    const seisValue = seis.value;
+    const sieteValue = siete.value;
+
+   
+    if(unoValue === '1'){
+        setSuccessFor(uno);
+    } else{
+        setErrorFor(uno);
+    }
+    
+    if(dosValue === '3'){
+        setSuccessFor(dos);
+    } else{
+        setErrorFor(dos);
+    }
+
+    if(tresValue === '7'){
+        setSuccessFor(tres);
+    } else{
+        setErrorFor(tres);
+    }
+
+    if(cuatroValue === '5'){
+        setSuccessFor(cuatro);
+    } else{
+        setErrorFor(cuatro);
+    }
+    
+    if(cincoValue === '6'){
+        setSuccessFor(cinco);
+    } else{
+        setErrorFor(cinco);
+    }
+     
+    if(seisValue === '2'){
+        setSuccessFor(seis);
+    } else{
+        setErrorFor(seis);
+    }
+     
+    if(sieteValue === '4'){
+        setSuccessFor(siete);
+    } else{
+        setErrorFor(siete);
+    }
+}
+function setErrorFor(input){
+    const formControl = input.parentElement;
+    formControl.className = 'form-control error';
+}
+
+function setSuccessFor(input){
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
+}
