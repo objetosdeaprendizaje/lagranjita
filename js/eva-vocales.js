199361db7e8fb4a5b5f1b3e71d1e6c067acc442c
objetosdeaprendizaje <daniela.na1208@gmail.com>
2021-08-28 23:40:36 -0500

    a dormir

---
+++ js/eva-vocales.js
@@ -0,0 +1,72 @@
+const form = document.getElementById('form');
+var uno = document.getElementById('uno');
+const dos = document.getElementById('dos');
+const tres = document.getElementById('tres');
+const cuatro = document.getElementById('cuatro');
+const cinco = document.getElementById('cinco');
+
+
+form.addEventListener('submit', (e) =>{
+    e.preventDefault();
+
+    checkInputs();
+});
+
+function checkInputs(){
+    var unoValue = uno.value;
+    const dosValue = dos.value;
+    const tresValue = tres.value;
+    const cuatroValue = cuatro.value;
+    const cincoValue = cinco.value;
+   
+    if(unoValue === '3'){
+        setSuccessFor(uno);
+    } else{
+        setErrorFor(uno);
+    }
+    
+    if(dosValue === '1'){
+        setSuccessFor(dos);
+    } else{
+        setErrorFor(dos);
+    }
+
+    if(tresValue === '2'){
+        setSuccessFor(tres);
+    } else{
+        setErrorFor(tres);
+    }
+
+    if(cuatroValue === '5'){
+        setSuccessFor(cuatro);
+    } else{
+        setErrorFor(cuatro);
+    }
+    
+    if(cincoValue === '4'){
+        setSuccessFor(cinco);
+    } else{
+        setErrorFor(cinco);
+    }
+     
+    if(seisValue === '5'){
+        setSuccessFor(seis);
+    } else{
+        setErrorFor(seis);
+    }
+     
+    if(sieteValue === '6'){
+        setSuccessFor(siete);
+    } else{
+        setErrorFor(siete);
+    }
+}
+function setErrorFor(input){
+    const formControl = input.parentElement;
+    formControl.className = 'form-control error';
+}
+
+function setSuccessFor(input){
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
+}
